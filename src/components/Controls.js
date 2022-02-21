b7f096b4bf3aea82814691b6d79a189c20800370
nathanaeng <nathaneng89@gmail.com>
2022-02-21 14:59:55 -0800

    Formatting updates and fixed play button render lag

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,17 +1,15 @@
-import React, { useLayoutEffect } from 'react';
+import React, { useEffect } from 'react';
 import { BsFillSkipStartFill, BsFillSkipEndFill } from 'react-icons/bs';
 import { GrRefresh } from 'react-icons/gr';
 
 const Controls = ({ back, play, next, speed, toggleSpeed, reset, playing }) => {
-  useLayoutEffect(() => {
+  useEffect(() => {
     if(playing) {
-      document.querySelector('.play').classList.add('pause');
+      document.querySelector('.play').innerHTML = '<svg width="25px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M272 63.1l-32 0c-26.51 0-48 21.49-48 47.1v288c0 26.51 21.49 48 48 48L272 448c26.51 0 48-21.49 48-48v-288C320 85.49 298.5 63.1 272 63.1zM80 63.1l-32 0c-26.51 0-48 21.49-48 48v288C0 426.5 21.49 448 48 448l32 0c26.51 0 48-21.49 48-48v-288C128 85.49 106.5 63.1 80 63.1z"/></svg>';
     } else {
-      document.querySelector('.play').classList.remove('pause');
+      document.querySelector('.play').innerHTML = '<svg width="25px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M361 215C375.3 223.8 384 239.3 384 256C384 272.7 375.3 288.2 361 296.1L73.03 472.1C58.21 482 39.66 482.4 24.52 473.9C9.377 465.4 0 449.4 0 432V80C0 62.64 9.377 46.63 24.52 38.13C39.66 29.64 58.21 29.99 73.03 39.04L361 215z"/></svg>';
     }
-
-    document.querySelector('.controls').onclick = disableHighlight;
-  });
+  }, [playing]);
 
   function disableHighlight() {
     document.body.classList.add('no-highlight');
@@ -21,7 +19,7 @@ const Controls = ({ back, play, next, speed, toggleSpeed, reset, playing }) => {
   }
 
   return(
-    <div className="center">
+    <div className="center" onClick={disableHighlight}>
         <div className="controls">
             <button className="reset" onClick={reset}><GrRefresh/></button>
             <button className="prev" onClick={back}><BsFillSkipStartFill style={{fontSize: "40px"}}/></button>
--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -2,6 +2,21 @@ import React from 'react';
 
 const Timeline = ({ index, length }) => {
     const total = (length-1)**2;
+
+    // function scrub(e) {
+    //     if(!document.querySelector('.no-alg')) {
+    //         let pos = e.clientX;
+    //         let width = e.target.getBoundingClientRect().width;
+    //         let ratio = (pos / width) - 0.05;
+    //         let goTo = Math.floor(length * ratio);
+    //         if(goTo === length) goTo = length-1;
+
+    //         setIndex(goTo);
+
+    //         // e.stopImmediatePropagation();
+    //     }
+    // }
+
     return(
         <div className="center">
             <div className="timeline">
